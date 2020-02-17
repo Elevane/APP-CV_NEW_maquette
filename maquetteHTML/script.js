1d4c240ad45ca54acf92a4a8a3c563688dce0c7e
AUBRY Bastien <Bastien.AUBRY@avem-groupe.com>
2020-02-17 17:23:43 +0100

    css : title bigger, menu on the left

---
+++ maquetteHTML/script.js
@@ -0,0 +1,55 @@
+let about = document.getElementById('about');
+let skills = document.getElementById('skills');
+let projects = document.getElementById('projects');
+let blog = document.getElementById('blog');
+let contact = document.getElementById('contact');
+
+
+var btnabout = document.getElementById('aabout');
+btnabout.addEventListener('click', function(){
+	about.scrollIntoView({behavior: "smooth"});
+})
+
+var btnskills = document.getElementById('askills');
+btnskills.addEventListener('click', function(){
+	skills.scrollIntoView({behavior: "smooth"});
+})
+
+var btnscontactmebtn = document.getElementById('contactmebtn');
+btnscontactmebtn.addEventListener('click', function () {
+	contact.scrollIntoView({ behavior: "smooth" });
+})
+
+var btnprojects = document.getElementById('aprojects');
+btnprojects.addEventListener('click', function(){
+	projects.scrollIntoView({behavior: "smooth"});
+})
+
+var btnblog = document.getElementById('ablog');
+btnblog.addEventListener('click', function(){
+	blog.scrollIntoView({behavior: "smooth"});
+})
+
+var btncontact = document.getElementById('acontact');
+ btncontact.addEventListener('click', function(){
+	contact.scrollIntoView({behavior: "smooth"});
+})
+
+
+$('.btnT').on('click', function(){
+   
+   if($(this).hasClass("unativeButton")){
+   	console.log("unactive");
+   	$(this).removeClass("unativeButton");
+   	$(this).addClass("activeButton");
+   }
+   else if(!$(this).hasClass("unativeButton")){
+	   console.log("ative");
+	   $(this).removeClass("activeButton");
+	   $(this).addClass("unativeButton");
+   }
+
+  });
+
+
+  
