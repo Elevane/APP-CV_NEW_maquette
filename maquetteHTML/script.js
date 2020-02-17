9d558216e0eb832025f9e663821ef69c4f2450c7
Elevane <pma.bastien.aubry@gmail.com>
2020-02-18 00:27:52 +0100

    css : moving the nav bar and starting javascript

--- maquetteHTML/script.js
+++ maquetteHTML/script.js
@@ -7,45 +7,59 @@ let contact = document.getElementById('contact');
 
 var btnabout = document.getElementById('aabout');
 btnabout.addEventListener('click', function(){
-	about.scrollIntoView({behavior: "smooth"});
+    about.scrollIntoView({behavior: "smooth"});
 })
 
 var btnskills = document.getElementById('askills');
 btnskills.addEventListener('click', function(){
-	skills.scrollIntoView({behavior: "smooth"});
+    skills.scrollIntoView({behavior: "smooth"});
+})
+
+var btnscontactmebtn = document.getElementById('contactmebtn');
+btnscontactmebtn.addEventListener('click', function () {
+    contact.scrollIntoView({ behavior: "smooth" });
 })
 
 var btnprojects = document.getElementById('aprojects');
 btnprojects.addEventListener('click', function(){
-	projects.scrollIntoView({behavior: "smooth"});
+    projects.scrollIntoView({behavior: "smooth"});
 })
 
 var btnblog = document.getElementById('ablog');
 btnblog.addEventListener('click', function(){
-	blog.scrollIntoView({behavior: "smooth"});
+    blog.scrollIntoView({behavior: "smooth"});
 })
 
 var btncontact = document.getElementById('acontact');
  btncontact.addEventListener('click', function(){
-	contact.scrollIntoView({behavior: "smooth"});
+    contact.scrollIntoView({behavior: "smooth"});
 })
 
 
-$('.btnT').on('click', function(){
+/*$('.btnT').on('click', function(){
    
    if($(this).hasClass("unativeButton")){
-   	console.log("unactive");
-   	$(this).removeClass("unativeButton");
-   	$(this).addClass("activeButton");
+    console.log("unactive");
+    $(this).removeClass("unativeButton");
+    $(this).addClass("activeButton");
    }
    else if(!$(this).hasClass("unativeButton")){
-	   console.log("ative");
-	   $(this).removeClass("activeButton");
-	   $(this).addClass("unativeButton");
+       console.log("ative");
+       $(this).removeClass("activeButton");
+       $(this).addClass("unativeButton");
    }
 
-  });
+  });*/
 
+var showmorePro = document.getElementById('showmorePro');
+
+showmorePro.addEventListener('click', function () {
+    var section = document.getElementById('projects');
+    var article = document.getElementById('ulpro');
+    section.style.height = parseInt(article.scrollHeight)+200+"px";
+    console.log(article.scrollHeight);
+    console.log('zdqz');
+})
 
 function adjust_textarea(h) {
 	h.style.height = "20px";
